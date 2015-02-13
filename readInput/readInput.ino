cb4f3c35890bcd2c986dd764f8a9fe911baf4e4c
Katherine Walker <kvwalker@users.noreply.github.com>
2015-02-12 20:29:16 -0500

    Update readInput.ino

--- readInput/readInput.ino
+++ readInput/readInput.ino
@@ -1,17 +1,15 @@
 int lastRow=1; //button pressed originally
 int lastColumn=1; //button pressed originally
-int rowArray[20]; //not needed
-int columnArray[20]; //not needed
 int rowCount=0; //when 20, confirm that button was pressed
 int columnCount=0; //when 20, confirm that button was pressed
-int scan=0;
+int scan=0; //count to keep track of when button was pressed
 
 char mapping[4][3] = {
                      {'#','0','*'},
                      {'9','8','7'},
                      {'6','5','4'},
                      {'3','2','1'}
-                     }; //2D array of keypad [opposite of keypad because this is how pins are set up]
+                     }; //2D character array of keypad [opposite of keypad because this is how pins are set up]
 
 // the setup function runs once when you press reset or power the board
 void setup() {
@@ -33,27 +31,33 @@ void setup() {
   //| 7 ] 4 | 5 | 6 |
   //| 6 ] 7 | 8 | 9 |
   //| 5 ] * | 0 | # |
+  
+   /* The above diagram depicts how the pins correspond to rows and columns. Pin 4 corresponds to column 1 4 7 *.
+     Pin 8 corresponds to row 1 2 3. 
+  */
 }
 
-// the loop function runs over and over again forever
+//this function runs continuously 
 void loop() {
-  //run through every column
-  for(int i=2;i<5;i++){
-    digitalWrite(((i-1)%3)+2,HIGH);
+  //run through every column, starting with pin 2 (column 3 6 9 #) and ending with pin 4 (column 1 4 7 *)
+  for(int i=2;i<5;i++)
+  {
+    digitalWrite(((i-1)%3)+2,HIGH); //write high to the pins that are not the current column
     digitalWrite((i%3)+2,HIGH);
-    digitalWrite(i,LOW);
-    //when one column on, run through every row
+    digitalWrite(i,LOW);  //write low to the pin that is the current column
+     //when one column is on, run through every row
+    //start with pin 5 (row * 0 #) and end with pin 8 (row 1 2 3)
     for(int j=5;j<9;j++){
       if(digitalRead(j) == 0){ //when digitalRead(j)=0, the button was pressed
-       scan = 0;
+       scan = 0; //if a button is pressed right now, start the count over
        printNum(j,i);
       }else{
-       if(scan == 12){
+       if(scan == 12){ //if no button has been pressed, set lastRow and lastColumn to 1
        lastRow = 1;
        lastColumn = 1;
-       }else{
+       }else{ //if a button has been pressed but is not currently pressed, increment the count
        scan++;
-     } 
+       } 
       }
     }
   }
@@ -61,11 +65,12 @@ void loop() {
 
 
 void printNum(int row, int column){
-  delay(10);
-  //Serial.println(mapping[row-5][column-2]);
-  if(row != lastRow || column !=lastColumn){
-    lastRow = row;
-    lastColumn = column;
-    Serial.println(mapping[row-5][column-2]);
+  delay(10); //wait to make sure correct key was pressed [debouncing
+  if(row != lastRow || column !=lastColumn){ //if row and column are valid and different from the previous values
+    lastRow = row; //set lastrow to the current row
+    lastColumn = column; //set lastcolumn to the current column
+    Serial.println(mapping[row-5][column-2]); //print the number corresponding to the correct column and row
+    //column - 2 because pins 2 - 4 are the columns
+    //row - 5 because pins 5 - 8 are the rows
   }
 }
